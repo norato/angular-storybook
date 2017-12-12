750f32e6783678991e6399a0bc808d0ee2de6ce7
Felipe Norato <flacerda@daitangroup.com>
2017-12-12 17:32:56 -0200

    Storybook is working

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import { configure } from '@storybook/angular';
+
+function loadStories() {
+  require('../src/stories');
+}
+
+configure(loadStories, module);
